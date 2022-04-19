f66c896bce8fe1630f5c9e467b61afc10819b5c6
Houston-Smith <houstonbsmith@gmail.com>
2022-04-19 10:15:20 -0500

    User Views Their own and their Friends Articles

--- src/components/article/ArticleList.js
+++ src/components/article/ArticleList.js
@@ -4,7 +4,7 @@ import { ArticleCard } from './ArticleCard';
 import { getAllArticles, getArticleById, deleteArticle, addArticle, getAllFriends } from './ArticleManager';
 import { useNavigate } from 'react-router-dom';
 import "./ArticleList.css"
-import { getFriendsOfActiveUser } from '../friends/FriendManager';
+import { getFriendsOfActiveUser } from '../../modules/FriendManager';
 
 export const ArticleList = () => {
 
@@ -19,69 +19,39 @@ export const ArticleList = () => {
 //----------------------------------------RETRIEVES ARTICLES FROM API AND FILTERS THEM BY USER---------------------------------------//
 
     const getArticles = () => {
-       
-        getAllArticles().then(articlesFromAPI => {
 
-            const filteredArticles = filterArticles(articlesFromAPI)
-            setArticles(filteredArticles)
+        let ArticlesList = []
+       
+        return getAllArticles().then(articlesFromAPI => {
+
+            console.log(articlesFromAPI)
+
+            getFriendsOfActiveUser(currentUser).then(friends =>{
+
+              const userIds = [currentUser, friends.map(f => f.theirId)].flat()
+              console.log(userIds)
+
+              articlesFromAPI.map(evt => {
+          
+                // if the event is posted by the active user or a friend 
+                // of the active user then add the event to the array
+                if (userIds.includes(evt.userId)) {
+                  ArticlesList.push(evt)
+                }
+            })            
+            console.log(ArticlesList)
+            setArticles(ArticlesList)
         });
         
-    };
+    })
 
-    const filterArticles = (articles) => {
-      const filteredByUser = articles.filter(article => article.userId === currentUser)
-      return(filteredByUser)
-    }
+  }
+
 
     useEffect(() => {
       getArticles();
     }, []);
 
-
-//----------------------------------------RETRIEVES FRIENDS FROM API AND FILTERS THEM BY USER---------------------------------------//    
-
-    const getFriends = () => {
-      getFriendsOfActiveUser(currentUser).then(friendsFromApi => {
-        setFriends(friendsFromApi)
-      })
-
-    }
-    
-    useEffect(() => {
-      getFriends()
-    }, []);
-
-
-//----------------------------------------RETRIEVES FRIENDS ARTICLES FROM THE API--------------------------------------------------//
-    
-const getFriendsArticles = () => {
-  
-  
-
-  getAllArticles().then(articlesFromAPI => {
-    
-    const articlesArray = []
-    const userIds = friends.map(f => f.theirId)
-    console.log(userIds)
-    articlesFromAPI.map(article => {
-      // if the event is posted by the active user or a friend 
-      // of the active user then add the event to the array
-      if (userIds.includes(article.userId)) {
-        articlesArray.push(article)
-      }
-  
-    });
-    console.log(articlesArray)
-    setFriendsArticles(articlesArray)
-  });
-};
-
-useEffect(() => {
-  getFriendsArticles()
-}, []);
-
-
-//----------------------------------------------------------------------------------------------------------------------------------//
 
     const handleDeleteArticle = id => {
         deleteArticle(id)
@@ -96,16 +66,8 @@ useEffect(() => {
     <h1>Articles</h1>
     <button type='button' className="btn btn-primary" onClick={() => {navigate("/articles/add")}}>Add new article</button>
     </section>
-    <h2>My Articles</h2>
     <div className="card-container">
       {articles.map(article =>
-        <ArticleCard
-          key={article.id}
-          article={article}
-          handleDeleteArticle={handleDeleteArticle} />)}
-    </div>
-    <div className="card-container">
-      {friendsArticles.map(article =>
         <ArticleCard
           key={article.id}
           article={article}
